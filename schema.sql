dcc0a023db14d24e85c902dfdcb044184b77a944
Gus-Daka <daka863@rambler.ru>
2018-05-07 21:40:24 +0300

    Задание 9. Схема БД. Исправления

--- schema.sql
+++ schema.sql
@@ -7,8 +7,8 @@ CREATE TABLE category (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE lots (
-  lot_id int(11) NOT NULL,
-  creat_date datetime NOT NULL,
+  id int(11) NOT NULL,
+  created_at int(11) NOT NULL,
   category_id int(10) UNSIGNED NOT NULL,
   user_id int(10) UNSIGNED NOT NULL,
   rate_win_id int(10) UNSIGNED NOT NULL,
@@ -21,7 +21,7 @@ CREATE TABLE lots (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE rate (
-  rate_id int(11) NOT NULL,
+  id int(11) NOT NULL,
   user_id int(10) UNSIGNED NOT NULL,
   lot_id int(10) UNSIGNED NOT NULL,
   rate_date datetime NOT NULL,
@@ -30,10 +30,10 @@ CREATE TABLE rate (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE users (
-  user_id int(11) NOT NULL,
+  id int(11) NOT NULL,
   reg_date datetime NOT NULL,
   email char(255) NOT NULL,
-  user_name tinytext NOT NULL,
+  user_name varchar(225) NOT NULL,
   password char(64) NOT NULL,
   user_foto tinytext NOT NULL,
   contact tinytext NOT NULL,
@@ -44,34 +44,32 @@ CREATE TABLE users (
 
 ALTER TABLE category
   ADD PRIMARY KEY (id),
-  ADD UNIQUE KEY cat_name (cat_name),
-  ADD KEY cat_name_2 (cat_name) USING BTREE;
+  ADD UNIQUE KEY cat_name (cat_name);
 
 ALTER TABLE lots
-  ADD PRIMARY KEY (lot_id),
-  ADD UNIQUE KEY creat_date (creat_date),
+  ADD PRIMARY KEY (id),
+  ADD UNIQUE KEY created_at (created_at),
   ADD UNIQUE KEY lot_name (lot_name);
 
 ALTER TABLE rate
-  ADD PRIMARY KEY (rate_id),
+  ADD PRIMARY KEY (id),
   ADD UNIQUE KEY rate_date (rate_date),
-  ADD KEY rate_win_id (rate_win_id),
-  ADD KEY rate_win_id_2 (rate_win_id);
+  ADD KEY rate_win_id (rate_win_id);
 
 ALTER TABLE users
-  ADD PRIMARY KEY (user_id),
+  ADD PRIMARY KEY (id),
   ADD UNIQUE KEY email (email);
 
 
 ALTER TABLE lots
-  ADD CONSTRAINT lots_ibfk_1 FOREIGN KEY (lot_id) REFERENCES `users` (user_id) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT lots_ibfk_2 FOREIGN KEY (lot_id) REFERENCES category (id) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT lots_ibfk_3 FOREIGN KEY (lot_id) REFERENCES rate (rate_id) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT lots_ibfk_1 FOREIGN KEY (id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT lots_ibfk_2 FOREIGN KEY (id) REFERENCES category (id) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT lots_ibfk_3 FOREIGN KEY (id) REFERENCES rate (id) ON DELETE CASCADE ON UPDATE CASCADE;
 
 ALTER TABLE rate
-  ADD CONSTRAINT rate_ibfk_1 FOREIGN KEY (rate_id) REFERENCES `users` (user_id) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT rate_ibfk_2 FOREIGN KEY (rate_id) REFERENCES lots (lot_id);
+  ADD CONSTRAINT rate_ibfk_1 FOREIGN KEY (id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT rate_ibfk_2 FOREIGN KEY (id) REFERENCES lots (id);
 
 ALTER TABLE users
-  ADD CONSTRAINT users_ibfk_1 FOREIGN KEY (user_id) REFERENCES lots (lot_id) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT users_ibfk_2 FOREIGN KEY (user_id) REFERENCES rate (rate_id) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT users_ibfk_1 FOREIGN KEY (id) REFERENCES lots (id) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT users_ibfk_2 FOREIGN KEY (id) REFERENCES rate (id) ON DELETE CASCADE ON UPDATE CASCADE;
